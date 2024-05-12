83afc008b996d12d91ea01674b416b64f442d017
cary123456 <caryhero4330@gmail.com>
2024-05-12 20:05:07 +0800

    5/12update licap 、 midicc
    licap 、 midicc加入(待測試)

---
+++ taichung/Assets/CCC/ccout.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ccout : MonoBehaviour
+{
+    public MidiChannel channel = MidiChannel.Ch1;
+    public int noteNumber = 48;
+    public float velocity = 0.9f;
+    public float delay = 2.0f;
+    public float length = 0.1f;
+    public float interval = 1.0f;
+    public float value;
+    public int controllerNumber = 64;
+    private float timer = 0f; // 计时器
+    public float vaa;
+    float scale;
+
+    public bool tri;
+
+    public void Update()
+    {
+        // 更新计时器
+        if (tri)
+        {
+            timer -= Time.deltaTime;
+
+            // 计算当前值，使用线性插值从0到1
+            float value = Mathf.Clamp01(timer / 10f);
+
+            // 应用值到你想要的地方，例如改变物体的颜色、大小等等
+            // 这里仅作示例，你可以根据你的需要进行修改
+            Debug.Log("Current value: " + value);
+
+            // 如果超过了3秒，重置计时器
+            if (timer <= 0f)
+            {
+                timer = 0f;
+            }
+            MidiBridge.instance.Warmup();
+            MidiOut.SendControlChange(channel, controllerNumber, value);
+        }
+        else
+        {
+            timer += Time.deltaTime;
+
+            // 计算当前值，使用线性插值从0到1
+            float value = Mathf.Clamp01(timer / 10f);
+
+            // 应用值到你想要的地方，例如改变物体的颜色、大小等等
+            // 这里仅作示例，你可以根据你的需要进行修改
+            Debug.Log("Current value: " + value);
+            if (Input.GetKeyUp(KeyCode.S))
+            {
+                timer = 0;
+                value = 0;
+            }
+            // 如果超过了3秒，重置计时器
+            if (timer >= 10f)
+            {
+                timer = 10f;
+            }
+            MidiBridge.instance.Warmup();
+            MidiOut.SendControlChange(channel, controllerNumber, vaa);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            tri = true;
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            tri = false;
+        }
+
+        
+    }
+   
+}
---
+++ taichung/Assets/LiCAP/FlowingStringControl.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class FlowingStringControl : MonoBehaviour
+{
+    //VisualEffect vfxPack;
+
+    [SerializeField]
+    int _chID = 0;
+    public int chID
+    {
+        get { return _chID; }
+        set { _chID = value; }
+    }
+
+    [SerializeField]
+    float _chOn0 = 0;
+    public float chOn0
+    {
+        get { return _chOn0; }
+        set { _chOn0 = value; }
+    }
+
+    [SerializeField]
+    float _chOn1 = 0;
+    public float chOn1
+    {
+        get { return _chOn1; }
+        set { _chOn1 = value; }
+    }
+
+    [SerializeField]
+    float _chOn2 = 0;
+    public float chOn2
+    {
+        get { return _chOn2; }
+        set { _chOn2 = value; }
+    }
+
+    [SerializeField]
+    float _chOn3 = 0;
+    public float chOn3
+    {
+        get { return _chOn3; }
+        set { _chOn3 = value; }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         //DontDestroyOnLoad(this.gameObject);
+        //vfxPack = GetComponent<VisualEffect>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+}
